893640236620d55f515b29339a27a6b42091eccc
Sila Klanklaeo <p_taung@hotmail.com>
2020-03-05 14:12:47 +0700

    Update params.php

--- config/params.php
+++ config/params.php
@@ -4,7 +4,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'lineToken' => 'Wh8N7xkvlQ2HqwN6V3Y9Gyq7SQf66xjirhL3xCTVh90', //Token For Line Api
+    'lineToken' => '', //Token For Line Api
     'bsVersion' => '4.x',
     'bsDependencyEnabled' => false,
 ];
